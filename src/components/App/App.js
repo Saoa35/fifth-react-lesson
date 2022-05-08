c27353d60b854d2e931f8de9df7ee6e68c961b49
Saoa35 <saoamah@gmail.com>
2022-05-08 20:58:15 +0200

    +propType

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import ControledCompExamp from '../ControledCompExamp/ControledCompExamp';
+import PropTypesExample from '../PropTypesExample/PropTypesExample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ControledCompExamp/>
+      {/* <ControledCompExamp/> */}
+
+      <PropTypesExample name='Alex' age={37}/>
     </div>
   );
 }
