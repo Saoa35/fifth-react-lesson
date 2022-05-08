5d5947cf235f77d08fa23b8b2e03dc78823f27d5
Saoa35 <saoamah@gmail.com>
2022-05-08 14:51:45 +0200

    + state @ metods

--- src/components/ControledCompExamp/ControledCompExamp.js
+++ src/components/ControledCompExamp/ControledCompExamp.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 class ControledCompExamp extends React.Component {
 
+    state = {
+        value: 'type your text'
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.setState({
+            value
+        })
+    }
+
     render() {
 
         return(
             <div>
-                ControledCompExamp
+                
+                <form>
+                    <input type='text' value={this.state.value} onChange={this.handleChange}/>
+                </form>
+
             </div>
         )
     }
