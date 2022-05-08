2baaeba11b13d761b1d967f0333bb306f593fcc4
Saoa35 <saoamah@gmail.com>
2022-05-08 14:59:22 +0200

    +handleSubmit

--- src/components/ControledCompExamp/ControledCompExamp.js
+++ src/components/ControledCompExamp/ControledCompExamp.js
@@ -3,28 +3,38 @@ import React from 'react';
 class ControledCompExamp extends React.Component {
 
     state = {
-        value: 'type your text'
+        value: 'type your text here'
     }
 
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
         const value = e.target.value;
         this.setState({
             value
-        })
+        });
+    }
+
+    handleSubmit(e) {
+        console.log(this.state.value);
+        e.preventDefault();
     }
 
     render() {
 
+
         return(
             <div>
                 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type='text' value={this.state.value} onChange={this.handleChange}/>
+                    <button>
+                        Get value
+                    </button>
                 </form>
 
             </div>
