0661ba05457b0a5e432d72bcc01c5839e05a6a89
Saoa35 <saoamah@gmail.com>
2022-05-08 14:06:51 +0200

    + import ControledCompExamp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
+import ControledCompExamp from '../ControledCompExamp/ControledCompExamp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ControledCompExamp/>
     </div>
   );
 }
