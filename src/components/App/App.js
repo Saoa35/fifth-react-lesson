b227094836666a4276d63f5c56ce7fee526ecaea
Saoa35 <saoamah@gmail.com>
2022-05-15 14:39:20 +0200

    + filtareble component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
 import ControledCompExamp from '../ControledCompExamp/ControledCompExamp';
+import FilterableProductTable from '../FiltarableProductTable';
 import PropTypesExample from '../PropTypesExample/PropTypesExample';
 import Calculator from '../TemperatureCalculator/TemperatureCalculator';
 import './App.css';
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 function App() {
   return (
@@ -11,6 +21,8 @@ function App() {
       <PropTypesExample name='Alex' age={37}/>
 
       {/* <Calculator/> */}
+
+      <FilterableProductTable products={PRODUCTS}/>
     </div>
   );
 }
